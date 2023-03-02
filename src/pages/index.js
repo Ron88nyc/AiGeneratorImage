1e379bc9452dd24d733764644f05770c8e8d5d5b
Ronald Li <Ronaldnyc88@gmail.com>
2023-03-02 16:50:44 -0500

    export created HomeCreate Post.Routes added

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+//this index file is use to serve as an export for the CreatePostjsx and Homejsx
+import Home from './Home';
+import CreatePost from './CreatePost';
+
+export {
+    Home,
+    CreatePost
+}
