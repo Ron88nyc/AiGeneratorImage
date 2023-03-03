01ae6734dce273bdedfa0bd9b1b44846c5822157
Ronald Li <Ronaldnyc88@gmail.com>
2023-03-03 14:13:23 -0500

    created server,installed mongo,cloudinary,npm

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -24,11 +24,12 @@ const CreatePost = () => {
   }
 
   const handleChange = (e) => {
-
+    setForm({ ...form, [e.target.name]: e.target.value})
   }
 
   const handleSurpriseMe = () => {
-
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt})
   }
 
   return (
@@ -95,6 +96,16 @@ const CreatePost = () => {
 
         </div>
 
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">Once you have created the image you want, you can share it with others in the community</p>
+        </div>
+        <button
+        type='submit'
+        className="mt-3 text-white bg-[#6469ff]
+        font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+        >
+          {loading ? 'Sharing...' : 'Share with the community'}
+        </button>
 
       </form>
 
