5ed8adafadae25fab72b2021aaa5b68fb28dc97e
Ronald Li <Ronaldnyc88@gmail.com>
2023-03-03 12:57:04 -0500

    src form for photo added

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -24,7 +24,7 @@ const CreatePost = () => {
   }
 
   const handleSurpriseMe = () => {
-    
+
   }
 
   return (
@@ -54,6 +54,23 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+                />
+            ) : (
+              <img
+              src={preview}
+              alt="preview"
+              className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+          </div>
           
           
         </div>
